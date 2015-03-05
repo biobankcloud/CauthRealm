231aa8480583a8f4c3d67f684b11b6b4d5b5ac9f
Ali Gholami <gholami@sics.se>
2015-03-05 15:44:27 +0100

    Changed the values os status

--- src/main/java/se/kth/bbc/crealm/CustomAuthRealm.java
+++ src/main/java/se/kth/bbc/crealm/CustomAuthRealm.java
@@ -476,7 +476,7 @@ public class CustomAuthRealm extends AppservRealm {
                         
                         valid = pwd.equalsIgnoreCase(hpwd);
                     } else {
-                        valid = pwd.equalsIgnoreCase(hpwd) && verifyCode(otp, Integer.parseInt(otpCode), getTimeIndex(), 5) && ((status == 1) || (status == 0));
+                        valid = pwd.equalsIgnoreCase(hpwd) && verifyCode(otp, Integer.parseInt(otpCode), getTimeIndex(), 5) && ((status == AccountStatus.ACCOUNT_ACTIVE) || (status == AccountStatus.ACCOUNT_PENDING));
                     }
                 } else {
                     // for only normal password
@@ -484,7 +484,7 @@ public class CustomAuthRealm extends AppservRealm {
                         _logger.info("## CustomAuthRealm disabled.");
                         valid = pwd.equalsIgnoreCase(hpwd);
                     } else {
-                        valid = pwd.equals(hpwd) && verifyCode(otp, Integer.parseInt(otpCode.trim()), getTimeIndex(), 5) && ((status == 1) || (status == 0));
+                        valid = pwd.equals(hpwd) && verifyCode(otp, Integer.parseInt(otpCode.trim()), getTimeIndex(), 5) && ((status == AccountStatus.ACCOUNT_ACTIVE) || (status == AccountStatus.ACCOUNT_PENDING));
                     }
                 }
             }
